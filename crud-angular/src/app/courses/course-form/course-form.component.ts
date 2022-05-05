3414873f00ded5167ab31dde4a00f167567a1aab
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2022-05-04 23:26:46 -0300

    Criando endpoint para salvar curso

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-import { CoursesService } from './../services/courses.service';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -14,28 +15,33 @@ export class CourseFormComponent implements OnInit {
   form: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
-              private service: CoursesService,
-              private snackBar: MatSnackBar) {
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
     });
-  }
+   }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(result => console.log(result), error => this.onError());
+      .subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
+    this.location.back();
+  }
 
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
   }
 
   private onError() {
-    this.snackBar.open('Erro ao salvar curso', '', {duration: 5000});
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 5000 });
   }
-
 }
--- crud-spring/src/main/java/com/rayana/controller/CourseController.java
+++ crud-spring/src/main/java/com/rayana/controller/CourseController.java
@@ -3,9 +3,9 @@ package com.rayana.controller;
 import com.rayana.model.Course;
 import com.rayana.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return repository.save(course);
+       /* return ResponseEntity.status(HttpStatus.CREATED)
+                .body(repository.save(course)); */
     }
 }
